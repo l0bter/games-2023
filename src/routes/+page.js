76d1341c2b16854bec1612b2a85d3dd599b5a007
DryCreations <DryCreations@gmail.com>
2022-10-10 11:18:22 -0400

    page client

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,8 +1,5 @@
 import { base } from '$app/paths';
 
-export const ssr = true
-export const csr = false
-
 export const load = async ({ fetch }) => {
     const response = await fetch(`${base}/api/games.json`)
     const games = await response.json()
